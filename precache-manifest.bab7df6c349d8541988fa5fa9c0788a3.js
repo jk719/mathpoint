daad85a6607358a9572a428dbbf908c71b28ab68
Jamil Kabir <jamilkabir719@gmail.com>
2024-04-18 19:08:16 -0400

    Updates

---
+++ precache-manifest.bab7df6c349d8541988fa5fa9c0788a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253254203a0a83f5318663c6c0b1bba6",
+    "url": "/mathpoint/index.html"
+  },
+  {
+    "revision": "7266ff0f045938bd5f79",
+    "url": "/mathpoint/static/css/main.8452d7ac.chunk.css"
+  },
+  {
+    "revision": "b355a2d4029bdf420e28",
+    "url": "/mathpoint/static/js/2.0533cb71.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/mathpoint/static/js/2.0533cb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54108c0585089323bd32",
+    "url": "/mathpoint/static/js/3.0b9449e4.chunk.js"
+  },
+  {
+    "revision": "7266ff0f045938bd5f79",
+    "url": "/mathpoint/static/js/main.bcf9ebdd.chunk.js"
+  },
+  {
+    "revision": "006e2539e431a5210e91",
+    "url": "/mathpoint/static/js/runtime-main.ad021a64.js"
+  },
+  {
+    "revision": "1220596e2b9158b18f57ea0c473d8f6c",
+    "url": "/mathpoint/static/media/logo.1220596e.png"
+  }
+]);
