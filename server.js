c5552be27ec6e1152c39ac3dd9e1525a469ca1d6
Jamil Kabir <jamilkabir719@gmail.com>
2024-05-09 18:41:12 -0400

    Set SameSite attribute for cookies

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 const app = express();
+
+const expiryDate = new Date(Date.now() + 60 * 60 * 24 * 1000); // 24 hours
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    domain: 'example.com',
+    path: 'foo/bar',
+    expires: expiryDate,
+    sameSite: 'lax' // or 'strict' or 'none'
+  }
+}));
 
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
